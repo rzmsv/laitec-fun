8175592cd7610ae54589206ed4d2ba681da6844a
rzmsv <rmussavi@gmail.com>
2020-11-18 16:36:15 +0330

    create mysql's tables (users,offers,get_offers).

--- db/tables.sql
+++ db/tables.sql
@@ -5,4 +5,27 @@ CREATE TABLE users (
      user VARCHAR(255) UNIQUE NOT NULL,
      email VARCHAR(255) UNIQUE NOT NULL,
      created_at TIMESTAMP DEFAULT NOW() 
-     );
+      );
+
+
+CREATE TABLE offers (
+     id INT AUTO_INCREMENT,
+     category VARCHAR(255) NOT NULL,
+     name VARCHAR(255) NOT NULL,
+     description VARCHAR(255) NOT NULL,
+     telephone INT NOT NULL,
+     image VARCHAR(255) NOT NULL,
+     address VARCHAR(255) NOT NULL,
+     off DECIMAL(3,1),
+     created_at TIMESTAMP DEFAULT NOW(),
+     PRIMARY KEY (id) 
+      );
+
+
+CREATE TABLE get_offers (
+     id INT PRIMARY KEY AUTO_INCREMENT,
+     user_id INT, offers_id INT,
+     FOREIGN KEY(user_id) REFERENCES users(id),
+     FOREIGN KEY(offers_id) REFERENCES offers(id),
+     created_at TIMESTAMP DEFAULT NOW()
+      );
